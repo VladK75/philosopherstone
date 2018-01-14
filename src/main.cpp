aa1ea3ba7932c1ee57a45972d17912c666b0767e
toxtox <schnickschnack3000@yahoo.com>
2018-01-14 22:37:27 +0100

    static linking of QT-libs requires Q_IMPORT_PLUGIN of "qxcb"

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,12 @@
 #include <boost/filesystem/fstream.hpp>
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
+
+
+#ifdef INCLUDE_STATIC_QXCB_PLUGIN_IN_MAIN_CPP
 #include <QtPlugin> // for static linking of Qt-libraries (libQt5Core.a and so on) "qxcb" plugin must be statically imported
 Q_IMPORT_PLUGIN (QXcbIntegrationPlugin);
+#endif
 
 
 using namespace std;
